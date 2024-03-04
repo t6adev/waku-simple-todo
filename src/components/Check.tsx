2302e2d4e404d17decd015f95bd76bbf2589f3c5
t6adev <t.yamamoto28@gmail.com>
2024-03-04 13:21:15 +0900

    chore

--- src/components/Check.tsx
+++ src/components/Check.tsx
@@ -14,7 +14,7 @@ export const Check = ({
     <form action={toggleFormAction} className="flex">
       <input
         type="checkbox"
-        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+        className="w-4 h-4"
         checked={done}
         onChange={(e) => {
           e.target.form?.requestSubmit();
