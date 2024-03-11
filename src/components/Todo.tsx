420c156519b048a7071807ce38980d18a6121618
t6adev <t.yamamoto28@gmail.com>
2024-03-11 11:31:35 +0900

    chore

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -2,8 +2,6 @@ import { Check } from './Check.js';
 import { toggleTodo, deleteTodo } from '../actions/todoActions.js';
 import { Delete } from './Delete.js';
 
-type ServerFunction<T> = T extends (...args: infer A) => infer R ? (...args: A) => R : never;
-
 export const Todo = ({ id, title, done }: { id: string; title: string; done: boolean }) => {
   return (
     <div className="flex gap-2">
@@ -11,11 +9,11 @@ export const Todo = ({ id, title, done }: { id: string; title: string; done: boo
         <Check
           id={id}
           done={done}
-          toggle={toggleTodo as unknown as ServerFunction<typeof toggleTodo>}
+          toggle={toggleTodo}
         />
         <div>{title}</div>
       </label>
-      <Delete id={id} deleteTodo={deleteTodo as unknown as ServerFunction<typeof deleteTodo>} />
+      <Delete id={id} deleteTodo={deleteTodo} />
     </div>
   );
 };
