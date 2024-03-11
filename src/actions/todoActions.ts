fdc8c99d70593766794d8eddc52aa210c31a9265
t6adev <t.yamamoto28@gmail.com>
2024-03-11 11:01:28 +0900

    update waku to 0.20.0-alpha.1

--- src/actions/todoActions.ts
+++ src/actions/todoActions.ts
@@ -1,34 +1,29 @@
 'use server';
 
 import { nanoid } from 'nanoid';
-import type { RenderContext } from 'waku/server';
+import { rerender } from 'waku/server';
 
 let todos = [{ id: nanoid(), title: 'buy milk', done: false }];
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export const readTodos = async () => todos;
-export async function addTodo(this: RenderContext, formData: FormData) {
+export const addTodo = async (formData: FormData) => {
   await sleep(2000);
   const title = formData.get('title');
   if (title === null) {
     throw new Error('Unexpected null title');
   }
   todos = [...todos, { title: title.toString(), id: nanoid(), done: false }];
-  this.rerender('/todo');
-}
-export async function deleteTodo(this: RenderContext, id: string) {
+  rerender('/todo');
+};
+export const deleteTodo = async (id: string) => {
   await sleep(2000);
   todos = todos.filter((todo) => todo.id !== id);
-  this.rerender('/todo');
-}
-export async function toggleTodo(
-  this: RenderContext,
-  id: string,
-  done: boolean
-  // formData: FormData
-) {
+  rerender('/todo');
+};
+export const toggleTodo = async (id: string, done: boolean) => {
   await sleep(2000);
   todos = todos.map((todo) => (todo.id === id ? { ...todo, done } : todo));
-  this.rerender('/todo');
-}
+  rerender('/todo');
+};
